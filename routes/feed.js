73105fe9ea5289689c12977bdac0fd4c37570456
gunavata <gunavata@hotmail.com>
2019-03-25 21:56:22 +0800

    362 Basics of rest Api, moving to advanced rest Api

---
+++ routes/feed.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const feedController = require('../controllers/feed.js')
+
+const router = express.Router();
+
+// GET /feed/posts
+router.get('/posts', feedController.getPosts);
+
+router.post('/post', feedController.createPost);
+
+module.exports = router;
