8133434428029f91fc45c4ac3af16352d2831bf2
gunavata <gunavata@hotmail.com>
2019-04-04 19:01:15 +0800

    421 Halfway through Graphql

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const graphqlHttp = require('express-graphql')
 
-const feedRoutes =  require('./routes/feed')
-const authRoutes =  require('./routes/auth')
-
+const graphqlSchema = require('./graphql/schema')
+const graphqlResolver = require('./graphql/resolvers')
 
 const app = express();
 
@@ -45,11 +45,26 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200)
+    }
     next();
 });
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
+app.use('/graphql', graphqlHttp({
+  schema: graphqlSchema,
+  rootValue: graphqlResolver,
+  graphiql: true,
+  formatError(err) {
+    if (!err.originalError) {
+      return err;
+    }
+    const data = err.originalError.data;
+    const message = err.message || 'An error occured!';
+    const code = err.originalError.code || 500;
+    return { message: message, status: code, data: data }
+  }
+}))
 
 app.use((error, req, res, next) => {
     console.log(error);
@@ -61,10 +76,6 @@ app.use((error, req, res, next) => {
 
 mongoose.connect(MONGO_URI)
 .then(result => {
-    const server = app.listen(8080);
-    const io = require('./socket').init(server);
-    io.on('connection', socket => {
-      console.log('Client Connected')
-    })
+    app.listen(8080);
 })
 .catch(err => console.log(err))
